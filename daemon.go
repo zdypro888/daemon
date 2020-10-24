642ab50db2b3e72a211923690bae904b1d07d2ce
Pro <43611116+zdypro888@users.noreply.github.com>
2020-10-24 13:35:26 +0800

    Add files via upload

---
+++ daemon.go
@@ -0,0 +1,104 @@
+package daemon
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"path"
+	"strings"
+	"syscall"
+
+	"github.com/kardianos/osext"
+	"github.com/takama/daemon"
+	"github.com/zdypro888/crash"
+)
+
+var gCrashFile, gLogFile *os.File
+
+//Run 运行
+func Run(name, description string, dependencies ...string) bool {
+	var flagInstall, flagRemove, flagStart, flagStop, flagStatus bool
+	flag.BoolVar(&flagInstall, "install", false, "install services")
+	flag.BoolVar(&flagRemove, "remove", false, "remove services")
+	flag.BoolVar(&flagStart, "start", false, "start services")
+	flag.BoolVar(&flagStop, "stop", false, "stop services")
+	flag.BoolVar(&flagStatus, "status", false, "show services status")
+	var flagArgs string
+	flag.StringVar(&flagArgs, "args", "", "args for services")
+	flag.Parse()
+	if flagInstall || flagRemove || flagStart || flagStop || flagStatus {
+		service, err := daemon.New(name, description, daemon.SystemDaemon, dependencies...)
+		if err != nil {
+			log.Printf("Init service faild: %v", err)
+		} else if flagInstall {
+			var args []string
+			if flagArgs != "" {
+				args = strings.Split(flagArgs, " ")
+			}
+			if result, err := service.Install(args...); err != nil {
+				log.Printf("Install faild(%s): %v", result, err)
+			} else {
+				log.Printf("Install success: %s", result)
+			}
+		} else if flagRemove {
+			if result, err := service.Remove(); err != nil {
+				log.Printf("Remove faild(%s): %v", result, err)
+			} else {
+				log.Printf("Remove success: %s", result)
+			}
+		} else if flagStart {
+			if result, err := service.Start(); err != nil {
+				log.Printf("Start faild(%s): %v", result, err)
+			} else {
+				log.Printf("Start success: %s", result)
+			}
+		} else if flagStop {
+			if result, err := service.Stop(); err != nil {
+				log.Printf("Stop faild(%s): %v", result, err)
+			} else {
+				log.Printf("Stop success: %s", result)
+			}
+		} else if flagStatus {
+			if result, err := service.Status(); err != nil {
+				log.Printf("Get status faild(%s): %v", result, err)
+			} else {
+				log.Printf("%s", result)
+			}
+		}
+		return false
+	}
+	return true
+}
+
+//RunWithConsole 运行
+func RunWithConsole(name, description string, dependencies ...string) bool {
+	if !Run(name, description, dependencies...) {
+		return false
+	}
+	folder, err := osext.ExecutableFolder()
+	if err != nil {
+		log.Printf("get executable folder faild: %v", err)
+		return false
+	}
+	if gCrashFile, err = crash.InitPanicFile(path.Join(folder, fmt.Sprintf("%s_crash.log", name))); err != nil {
+		log.Printf("open crash file faild: %v", err)
+		return false
+	}
+	if gLogFile, err = os.OpenFile(path.Join(folder, fmt.Sprintf("%s.log", name)), os.O_CREATE|os.O_WRONLY|os.O_TRUNC|os.O_SYNC, 0644); err != nil {
+		log.Printf("open log file faild: %v", err)
+		return false
+	}
+	log.SetOutput(gLogFile)
+	return true
+}
+
+//WaitNotify 等待信号量
+func WaitNotify() {
+	interrupt := make(chan os.Signal, 1)
+	defer close(interrupt)
+	signal.Notify(interrupt, os.Interrupt, os.Kill, syscall.SIGTERM)
+	killSignal := <-interrupt
+	log.Printf("interruped by system signal: %d", killSignal)
+}
